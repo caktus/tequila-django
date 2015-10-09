c9c7d5bbdf1a3244dcf53097b1bbb3661b9b0d34
Dan Poirier <dpoirier@caktusgroup.com>
2015-10-09 08:00:10 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+
+setup(
+    name='tequila',
+    version='0.0.1',
+    packages=['tequila'],
+    url='https://github.com/caktus/tequila/',
+    license='BSD',
+    author='Caktus Consulting Group',
+    author_email='info@caktusgroup.com',
+    description=''
+)

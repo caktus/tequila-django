555a9009d1e3e94816d67826391f0fbc1222c75c
dpoirier <dpoirier@caktusgroup.com>
2018-07-30 08:20:08 -0400

    Support django project in subdir, and a few other things
    * Allow tequila to work with a django repository where the manage.py,
      app dirs, etc are all in a subdir of the top directory of the
      repository.
    * Allow configuring the package path to the Django project's wsgi
      module.
    * A few changes so everywhere we run Django, we use the environment
      settings from the .env file that Tequila creates.
    * Add the directory to the path where the npm from geerlingguy.nodejs
      installs global programs.

--- templates/manage.sh
+++ templates/manage.sh
@@ -1,3 +1,3 @@
 # Shell script to run a management command
-cd {{ source_dir }}
-{{ venv_dir }}/bin/python {{ source_dir }}/manage.py $@
+cd {{ source_dir }}/{{ project_subdir }}
+./dotenv.sh {{ venv_dir }}/bin/python {{ source_dir }}/{{ project_subdir }}/manage.py $@
